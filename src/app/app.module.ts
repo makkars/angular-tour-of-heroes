8c3ef5f1cbd8520cbe724090b3d7867ef7eddad4
makkars <shashankmakkarwork@gmail.com>
2017-12-06 18:44:01 +0530

    Added asyn call to heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './component/heroes/heroes.component';
 import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
 import { HeroService } from './service/hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './service/message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [ HeroService ],
+  providers: [ HeroService, MessageService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
 import { Hero } from '../model/hero';
 import { HEROES } from '../model/mock-heroes';
 
@@ -7,7 +10,7 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
