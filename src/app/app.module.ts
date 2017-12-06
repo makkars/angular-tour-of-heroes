2e6bbe9efc3bdaaf42847bb4fbd337d072f6a32e
makkars <shashankmakkarwork@gmail.com>
2017-12-06 18:19:03 +0530

    master detail component view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './component/heroes/heroes.component';
+import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
